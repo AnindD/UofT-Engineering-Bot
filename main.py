5765146bf4826a372030d15d38432c78c33ccad9
AnindD <anindit.dewan@gmail.com>
2024-05-11 18:18:00 -0400

    Added pingloop feature. Allows user to ping user, n amount of times

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from typing import Final
 import os 
 from dotenv import load_dotenv
 from discord.ext import commands
-from discord import Intents, Client, Message
-
+from discord import Intents, Client, Message, Member
+import asyncio 
 # Load our individual token
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
@@ -40,5 +40,28 @@ async def on_message(message):
     if "Mohamed" in message.content:
         await channel.send("Mohamed, a man who enjoys the slumber of night")
     await bot.process_commands(message)
+
+
+@bot.command()
+async def Pingloop(ctx, member: Member = None):
+  if member == None:
+    member = ctx.author
+  try: 
+    await ctx.send("How many times do you want to ping this person? ")
+    num_Ping = await bot.wait_for("message", timeout=15, check=lambda message: message.author == ctx.author and message.channel == ctx.channel)
+  except asyncio.TimeoutError():
+    await ctx.send("You unfortunately ran out of time. ")
+  try: 
+    loop_Counter = int(num_Ping.content)
+  except: 
+    await ctx.send("This is not a real number")
+  if loop_Counter < 0:
+    await ctx.send("Negative number is invalid")
+  else: 
+    loop_Start = 0
+    while (loop_Start < loop_Counter):
+      await ctx.send(member.mention)
+      loop_Start = loop_Start + 1  
+
 
 bot.run(TOKEN)
